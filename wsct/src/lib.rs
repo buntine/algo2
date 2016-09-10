2123fabc2280cfcbfde9155debaef8d35127bcd2
Bunts Thy Unholy <bunts@hhd.com.au>
2016-09-10 20:15:22 +1000

    Implement ordering for Job

--- wsct/src/lib.rs
+++ wsct/src/lib.rs
@@ -1,21 +1,51 @@
 use std::path::Path;
 use std::io::BufReader;
 use std::io::BufRead;
-use std::error::Error;
 use std::fs::File;
+use std::cmp::Ordering;
 
 pub struct Job {
     weight: i32,
     length: i32,
 }
 
-pub fn wsct(jobs: &[Job]) -> i32 {
+impl Job {
+    fn value(&self) -> f32 {
+        self.weight as f32 / self.length as f32
+    }
+}
+
+impl Ord for Job {
+    fn cmp(&self, other: &Self) -> Ordering {
+        self.value()
+            .partial_cmp(&other.value())
+            .unwrap_or(Ordering::Equal)
+            .reverse()
+    }
+}
+
+impl PartialOrd for Job {
+    fn partial_cmp(&self, other: &Self) -> Option<Ordering> {
+        Some(self.cmp(other))
+    }
+}
+
+impl PartialEq for Job {
+    fn eq(&self, other: &Self) -> bool {
+        (self.weight, self.length) == (other.weight, other.length)
+    }
+}
+
+impl Eq for Job { }
+
+pub fn wsct(jobs: &mut Vec<Job>) -> i32 {
+    jobs.sort_by(|a, b| a.cmp(b));
     return jobs.len() as i32;
 }
 
 pub fn jobs_from_file(path: &Path) -> Result<Vec<Job>, std::io::Error> {
-    let mut file = try!(File::open(path));
-    let mut buffer = BufReader::new(&file);
+    let file = try!(File::open(path));
+    let buffer = BufReader::new(&file);
     let parse_job = |l: std::io::Result<String>| -> Job {
         let line = l.ok().unwrap();
         let details: Vec<i32> = line.split(" ")
@@ -42,7 +72,7 @@ mod tests {
     fn test1() {
         let p = Path::new("jobs.txt");
         let mut jobs: Vec<Job> = jobs_from_file(&p).ok().unwrap();
-        let s = wsct(&jobs[..]);
+        let s = wsct(&mut jobs);
 
         assert_eq!(s, 10000);
     }
