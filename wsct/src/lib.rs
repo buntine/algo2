01250df57ae1478193d41d8be2c80d3fa3a88ab8
Bunts Thy Unholy <bunts@hhd.com.au>
2016-09-10 18:39:59 +1000

    Shell for loading from file

--- wsct/src/lib.rs
+++ wsct/src/lib.rs
@@ -1,21 +1,29 @@
+use std::path::Path;
+
 pub struct Job {
     weight: i32,
     length: i32,
 }
 
 pub fn wsct(jobs: &[Job]) -> i32 {
-    return 10;
+    return jobs.len() as i32;
+}
+
+pub fn jobs_from_file(path: &Path) -> Result<Vec<Job>, String> {
+    return Ok(vec![Job{weight: 1, length: 2}]);
 }
 
 #[cfg(test)]
 mod tests {
     use super::*;
+    use std::path::Path;
 
     #[test]
     fn test1() {
-        let jobs: Vec<Job> = vec![];
+        let p = Path::new("jobs.txt");
+        let mut jobs: Vec<Job> = jobs_from_file(&p).ok().unwrap();
         let s = wsct(&jobs[..]);
 
-        assert_eq!(s, 10)
+        assert_eq!(s, 1)
     }
 }
