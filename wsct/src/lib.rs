0ec0df4f322a5b83cfd274f02137b848d1a02ca4
Bunts Thy Unholy <bunts@hhd.com.au>
2016-09-10 19:28:21 +1000

    Reading from file and parsing into correct data structure

--- wsct/src/lib.rs
+++ wsct/src/lib.rs
@@ -1,4 +1,8 @@
 use std::path::Path;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::error::Error;
+use std::fs::File;
 
 pub struct Job {
     weight: i32,
@@ -9,8 +13,24 @@ pub fn wsct(jobs: &[Job]) -> i32 {
     return jobs.len() as i32;
 }
 
-pub fn jobs_from_file(path: &Path) -> Result<Vec<Job>, String> {
-    return Ok(vec![Job{weight: 1, length: 2}]);
+pub fn jobs_from_file(path: &Path) -> Result<Vec<Job>, std::io::Error> {
+    let mut file = try!(File::open(path));
+    let mut buffer = BufReader::new(&file);
+    let parse_job = |l: std::io::Result<String>| -> Job {
+        let line = l.ok().unwrap();
+        let details: Vec<i32> = line.split(" ")
+                                     .map(|d| d.parse::<i32>().ok().unwrap())
+                                     .collect();
+
+        Job{weight: details[0], length: details[1]}
+    };
+
+    let jobs = buffer.lines()
+                     .skip(1)
+                     .map(parse_job)
+                     .collect();
+
+    return Ok(jobs);
 }
 
 #[cfg(test)]
@@ -24,6 +44,6 @@ mod tests {
         let mut jobs: Vec<Job> = jobs_from_file(&p).ok().unwrap();
         let s = wsct(&jobs[..]);
 
-        assert_eq!(s, 1)
+        assert_eq!(s, 10000);
     }
 }
