9a8fd939af0adf04116cf9338a3a267a97e5ad9f
Bunts Thy Unholy <bunts@hhd.com.au>
2016-09-10 18:22:13 +1000

    Initial setup

--- wsct/src/lib.rs
+++ wsct/src/lib.rs
@@ -1,6 +1,22 @@
+pub struct Job {
+    weight: i32,
+    length: i32,
+}
+
+pub fn wsct(jobs: &[Job]) -> i32 {
+    return 10;
+}
+
 #[cfg(test)]
 mod tests {
+    use super::*;
+
     #[test]
-    fn it_works() {
+    fn test1() {
+        let size = 10;
+        let jobs: Vec<Job> = vec![];
+        let s = wsct(&jobs[..]);
+
+        assert_eq!(1, 1)
     }
 }
