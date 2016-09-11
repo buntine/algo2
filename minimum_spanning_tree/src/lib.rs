f53ec3fdf92d8cd41219254bd6a8014d26892180
Bunts Thy Unholy <bunts@hhd.com.au>
2016-09-11 11:58:10 +1000

    Hacked together a parse-from-file method

--- minimum_spanning_tree/src/lib.rs
+++ minimum_spanning_tree/src/lib.rs
@@ -1,3 +1,8 @@
+use std::path::Path;
+use std::io::BufReader;
+use std::io::BufRead;
+use std::fs::File;
+
 pub struct Graph {
     nodes: Vec<Vertex>,
 }
@@ -10,7 +15,7 @@ pub struct Vertex {
 
 pub struct Edge {
     cost: u32,
-    tail: i32,
+    tail: i32, // This should be a borrow of another Vertex but I couldn't work it out.
 }
 
 impl Graph {
@@ -18,6 +23,39 @@ impl Graph {
         for i in 0..size {
             self.nodes.push(Vertex{label: i, explored: false, adjacent: vec![]});
         }
+    }
+
+    fn split_line<T: std::str::FromStr>(line: &str) -> Vec<T> {
+        line.split(" ")
+            .map(|d| d.parse::<T>().ok().unwrap())
+            .collect()
+    }
+
+    fn from_file(path: &Path) -> Result<Graph, std::io::Error> {
+        let mut g = Graph{nodes: vec![]};
+        let file = try!(File::open(path));
+        let mut buffer = BufReader::new(&file);
+        let mut first_line = String::new();
+        
+        try!(buffer.read_line(&mut first_line));
+
+        let details = Graph::split_line::<i32>(&first_line[..]);
+
+        g.build(details[0]);
+
+        for l in buffer.lines().skip(1) {
+            match l {
+                Ok(parts) => {
+                    let details = Graph::split_line::<u32>(&parts[..]);
+                    let ref mut node = g.nodes[details[0] as usize];
+
+                    node.adjacent.push(Edge{tail: details[1] as i32, cost: details[2]});
+                },
+                Err(e) => return Err(e),
+            }
+        }
+ 
+        Ok(g)
     }
 }
 
